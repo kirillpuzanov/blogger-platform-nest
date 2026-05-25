6a7d2d0da815b269d7a7a68958d98a3fd6d6cf83
kir_sp <kirillpuzanov@mail.ru>
2026-05-25 18:20:22 +0300

    4-13 перевод модуля testing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { UsersModule } from './modules/users/users.module';
-import { settings } from './setup/settings';
+import { UsersModule } from '../modules/users/users.module';
+import { settings } from '../setup/settings';
+import { TestingModule } from '../modules/testing/testing.module';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { settings } from './setup/settings';
       dbName: settings.DB_NAME,
     }),
     UsersModule,
+    TestingModule,
   ],
   controllers: [AppController],
   providers: [AppService],
