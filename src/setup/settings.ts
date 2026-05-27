2a767a4d75ac321f4568c34ba133a2ac1fd4ff00
kir_sp <kirillpuzanov@mail.ru>
2026-05-27 21:05:09 +0300

    4-13 добавил secret в gitignore

--- src/setup/settings.ts
+++
@@ -1,9 +0,0 @@
-export const settings = {
-  PORT: process.env.PORT ?? 3001,
-  MONGO_URL: process.env.MONGO_URL ?? '_',
-  DB_NAME: process.env.DB_NAME ?? 'blockPostDb',
-  JWT_SECRET: process.env.JWT_SECRET ?? 'jwt_secret',
-  EMAIL: process.env.EMAIL ?? '',
-  EMAIL_PASS: process.env.EMAIL_PASS ?? '',
-  APP_GLOBAL_PREFIX: 'api',
-};
